eee6cf041dfaabff3e79c2fdf59698fdd520f606
bd8993 <bdonahue@socsoter.com>
2022-04-12 10:32:16 -0400

    fixed?

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,9 @@
 import { Link } from "@remix-run/react";
 import house from "~/Media/GameHouse.png";
+import { useOptionalUser } from "~/utils";
 
 const Navbar = () => {
+    const user = useOptionalUser();
     return(
         <nav className="flex flex-row w-full h-12 items-center px-4 justify-between">
             <Link className="h-8 flex flex-row gap-2 justify-center" to="/">
@@ -12,6 +14,29 @@ const Navbar = () => {
             <Link to="/territoryWars" className="text-slate-200 cursor-pointer hover:text-slate-50 hover:underline">
                 Territory Wars
             </Link>
+            {user ? (
+                  <Link
+                    to="/notes"
+                    className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-yellow-700 shadow-sm hover:bg-yellow-50 sm:px-8"
+                  >
+                    View Notes for {user.email}
+                  </Link>
+                ) : (
+                  <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
+                    <Link
+                      to="/join"
+                      className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-yellow-700 shadow-sm hover:bg-yellow-50 sm:px-8"
+                    >
+                      Sign up
+                    </Link>
+                    <Link
+                      to="/login"
+                      className="flex items-center justify-center rounded-md bg-yellow-500 px-4 py-3 font-medium text-white hover:bg-yellow-600  "
+                    >
+                      Log In
+                    </Link>
+                  </div>
+                )}
             </div>
         </nav>
     )
